86cf5b5f21b31908f729eda58a8f1f34a600edc5
Raafia Fatima <raafia.fatima.2004@gmail.com>
2025-11-01 20:46:30 +0500

    Layout added

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -12,6 +12,41 @@ const _layout = () => {
               headerShown: false,
             }}
           />          
+          <Stack.Screen
+            name="(dashboard)"
+            options={{
+              title: "Dashoard",
+              headerShown: false,
+            }}
+          />  
+          <Stack.Screen
+            name="(inventory)"
+            options={{
+              title: "Inventory",
+              headerShown: false,
+            }}
+          />  
+          <Stack.Screen
+            name="(equipment)"
+            options={{
+              title: "Equipment",
+              headerShown: false,
+            }}
+          />  
+          <Stack.Screen
+            name="(market)"
+            options={{
+              title: "Market",
+              headerShown: false,
+            }}
+          />  
+          <Stack.Screen
+            name="(bulletin)"
+            options={{
+              title: "Bulletin",
+              headerShown: false,
+            }}
+          />  
         </Stack>
   );
 };
