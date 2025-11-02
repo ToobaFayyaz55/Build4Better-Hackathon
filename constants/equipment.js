4908f787c51958b77b639dabf223dbd16ff476ee
hishaque06 <hishaque06@gmail.com>
2025-11-02 07:17:06 +0500

    made constant file for equipment

---
+++ constants/equipment.js
@@ -0,0 +1,30 @@
+export const mockEquipment = [
+  {
+    id: "1",
+    name: "Tractor",
+    type: "Vehicle",
+    description: "A powerful tractor for heavy farming.",
+    // images: ['data:image/png;base64,...'], // put your base64 strings here
+  },
+  {
+    id: "2",
+    name: "Plough",
+    type: "Tool",
+    description: "Used to till the soil efficiently.",
+    images: ["data:image/png;base64,..."],
+  },
+  {
+    id: "3",
+    name: "Harvester",
+    type: "Vehicle",
+    description: "Used for harvesting crops quickly.",
+    // images: ['data:image/png;base64,...'],
+  },
+];
+
+export const mockEquipmentTypes = [
+  "Vehicle",
+  "Tool",
+  "Irrigation",
+  "Fertilizer",
+];
